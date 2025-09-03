6debace0cd0e0c71de3ede46c61fe78f35a1b9bb
umaagaa <umejr.mujagic@hotmail.com>
2025-09-03 16:29:39 +0200

    added section for about us on landing with numbers...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Hero from "../components/Hero";
 import FeaturesInline from "../components/FeaturesInline";
 import Gallery from "../components/Gallery";
 import Footer from "../components/Footer";
+import StatsSection from "../components/StatsSection";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <FeaturesInline />
       <Gallery />
+      <StatsSection />
       <Footer />
     </>
   );
